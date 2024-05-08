fddeae751befc9fc308f5353a2a388b03dc40677
Tim Burks <timburks@me.com>
2024-05-08 14:07:17 -0700

    Initial commit

---
+++ web-server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fs := http.FileServer(http.Dir("public"))
+	log.Fatal(http.ListenAndServe(":"+port, fs))
+}
